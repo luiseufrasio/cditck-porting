ec341157332da9d9384f422c549df4473de4e844
Alfonso Altamirano <alfonso.altamirano@payara.fish>
2024-07-29 12:05:40 -0600

    FISH-9109: adding improvements to execute cdi tck

---
+++ glassfish-tck-runner/src/test/java/org/jboss/weld/tck/glassfish/GlassFishBeansImpl.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2024 Eclipse Foundation and/or its affiliates.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+// Portions Copyright [2024] [Payara Foundation and/or its affiliates]
+package org.jboss.weld.tck.glassfish;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.jboss.cdi.tck.spi.Beans;
+
+public class GlassFishBeansImpl implements Beans {
+
+    private Object fakeSerialized;
+
+    @Override
+    public boolean isProxy(Object instance) {
+        return instance.getClass().getName().indexOf("_$$_Weld") > 0;
+    }
+
+    @Override
+    public byte[] passivate(Object instance) throws IOException {
+        fakeSerialized = instance;
+        return instance.toString().getBytes();
+    }
+
+    @Override
+    public Object activate(byte[] bytes) throws IOException, ClassNotFoundException {
+        if (Arrays.equals(fakeSerialized.toString().getBytes(), bytes)) {
+            Object result = fakeSerialized;
+            fakeSerialized = null;
+            return result;
+        }
+        return null;
+    }
+}
